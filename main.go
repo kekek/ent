17a865631d089ca637eed19a8ae804a21c1c43be
jinyf <jyf0231@163.com>
2021-08-13 18:31:08 +0800

    pu

--- main.go
+++ main.go
@@ -4,84 +4,90 @@
 
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-	"time"
-
-	"ent-todo/internal/ent"
-
-	_ "github.com/go-sql-driver/mysql"
-)
+import "fmt"
 
 func main() {
-	client, err := ent.Open("mysql", "test:test@tcp(10.50.23.194:3306)/ent?parseTime=true")
-	if err != nil {
-		log.Fatalf("failed opening connection to mysql: %v", err)
-	}
-	defer client.Close()
-
-	ctx := context.TODO()
-	err = Do(ctx, client)
-	if err != nil {
-		log.Fatalf("create info failed %v", err)
-	}
-
-	log.Println("create done")
+	fmt.Println("test protobuf plugin")
 }
 
-func Do(ctx context.Context, client *ent.Client) error {
-
-
-	name := fmt.Sprintf("Mashraki - %d", time.Now().Nanosecond())
-
-	a8m, err := client.User.
-		Create().
-		SetAge(30).
-		SetName(name).
-		SetUsername(name).
-		Save(ctx)
-	if err != nil {
-		return fmt.Errorf("creating user: %w", err)
-	}
-	log.Println("user:", a8m)
-	card1, err := client.Card.
-		Create().
-		SetOwner(a8m).
-		SetNumber("1020").
-		SetExpired(time.Now().Add(time.Minute)).
-		Save(ctx)
-	if err != nil {
-		return fmt.Errorf("creating card: %w", err)
-	}
-	log.Println("card:", card1)
-	// Only returns the card of the user,
-	// and expects that there's only one.
-	card2, err := a8m.QueryCard().Only(ctx)
-	if err != nil {
-		return fmt.Errorf("querying card: %w", err)
-	}
-	log.Println("card:", card2)
-	// The Card entity is able to query its owner using
-	// its back-reference.
-	owner, err := card2.QueryOwner().Only(ctx)
-	if err != nil {
-		return fmt.Errorf("querying owner: %w", err)
-	}
-	log.Println("owner:", owner)
-	return nil
-}
-
-
-func createTables()  {
-	client, err := ent.Open("mysql", "test:test@tcp(10.50.23.194:3306)/ent")
-	if err != nil {
-		log.Fatalf("failed opening connection to mysql: %v", err)
-	}
-	defer client.Close()
-	// Run the auto migration tool.
-	if err := client.Schema.Create(context.Background()); err != nil {
-		log.Fatalf("failed creating schema resources: %v", err)
-	}
-}
+//import (
+//	"context"
+//	"fmt"
+//	"log"
+//	"time"
+//
+//	"ent-todo/internal/ent"
+//
+//	_ "github.com/go-sql-driver/mysql"
+//)
+//
+//func main() {
+//	client, err := ent.Open("mysql", "test:test@tcp(10.50.23.194:3306)/ent?parseTime=true")
+//	if err != nil {
+//		log.Fatalf("failed opening connection to mysql: %v", err)
+//	}
+//	defer client.Close()
+//
+//	ctx := context.TODO()
+//	err = Do(ctx, client)
+//	if err != nil {
+//		log.Fatalf("create info failed %v", err)
+//	}
+//
+//	log.Println("create done")
+//}
+//
+//func Do(ctx context.Context, client *ent.Client) error {
+//
+//
+//	name := fmt.Sprintf("Mashraki - %d", time.Now().Nanosecond())
+//
+//	a8m, err := client.User.
+//		Create().
+//		SetAge(30).
+//		SetName(name).
+//		SetUsername(name).
+//		Save(ctx)
+//	if err != nil {
+//		return fmt.Errorf("creating user: %w", err)
+//	}
+//	log.Println("user:", a8m)
+//	card1, err := client.Card.
+//		Create().
+//		SetOwner(a8m).
+//		SetNumber("1020").
+//		SetExpired(time.Now().Add(time.Minute)).
+//		Save(ctx)
+//	if err != nil {
+//		return fmt.Errorf("creating card: %w", err)
+//	}
+//	log.Println("card:", card1)
+//	// Only returns the card of the user,
+//	// and expects that there's only one.
+//	card2, err := a8m.QueryCard().Only(ctx)
+//	if err != nil {
+//		return fmt.Errorf("querying card: %w", err)
+//	}
+//	log.Println("card:", card2)
+//	// The Card entity is able to query its owner using
+//	// its back-reference.
+//	owner, err := card2.QueryOwner().Only(ctx)
+//	if err != nil {
+//		return fmt.Errorf("querying owner: %w", err)
+//	}
+//	log.Println("owner:", owner)
+//	return nil
+//}
+//
+//
+//func createTables()  {
+//	client, err := ent.Open("mysql", "test:test@tcp(10.50.23.194:3306)/ent")
+//	if err != nil {
+//		log.Fatalf("failed opening connection to mysql: %v", err)
+//	}
+//	defer client.Close()
+//	// Run the auto migration tool.
+//	if err := client.Schema.Create(context.Background()); err != nil {
+//		log.Fatalf("failed creating schema resources: %v", err)
+//	}
+//}
